88adb1b88add6bca2088c75146885d1c3416138d
fmgrtndz <112537359+fmgrtndz@users.noreply.github.com>
2022-09-18 08:01:05 +0000

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -12,7 +12,7 @@ fi
 echo ${Vless_Path}
 
 if [[ -z "${Vless_UUID}" ]]; then
-  Vless_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vless_UUID="26190e7a-f0f5-4b24-a3d9-d650efdf7fa5"
 fi
 echo ${Vless_UUID}
 
@@ -22,7 +22,7 @@ fi
 echo ${Vmess_Path}
 
 if [[ -z "${Vmess_UUID}" ]]; then
-  Vmess_UUID="5c301bb8-6c77-41a0-a606-4ba11bbab084"
+  Vmess_UUID="26190e7a-f0f5-4b24-a3d9-d650efdf7fa5"
 fi
 echo ${Vmess_UUID}
 
